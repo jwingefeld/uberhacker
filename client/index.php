c40b0ec493e4bf40185370c7c5e01a889fdbc33f
Jan <jan.wingefeld@googlemail.com>
2016-06-21 14:09:06 +0200

    initial commit.. did some work before

---
+++ client/index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Multihack</title>
+  <meta name="author" content="Jan">
+
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/base.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+  <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro:400,900' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+  <ul id="iconbar">
+    <li class="fa fa-bomb active"><span>1</span></li>
+    <li class="fa fa-user-secret">
+      <span>2</span>
+    </li>
+    <li class="fa fa-building-o">
+      <span>3</span>
+    </li>
+    <li class="fa fa-globe">
+      <span>4</span>
+    </li>
+    <li class="fa fa-unlock-alt"></li>
+    <li class="fa fa-wifi"></li>
+    <li class="fa fa-warning"></li>
+    <li class="fa fa-shield"></li>
+
+  </ul>
+  <div class="window small unfocus ui-widget-content">
+    <div class="windowtitle">WindowTitle</div>
+    <div class="windowcontent">
+      WindowContentOfTheYear
+    </div>
+  </div>
+  <div class="window normal focus ui-widget-content">
+    <div class="windowtitle">WindowTitle</div>
+    <div class="windowcontent">
+      WindowContentOfTheYear
+    </div>
+  </div>
+  <div class="window large unfocus ui-widget-content">
+    <div class="windowtitle">WindowTitle</div>
+    <div class="windowcontent">
+      WindowContentOfTheYear
+    </div>
+  </div>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ client/js/scripts.js
@@ -0,0 +1,29 @@
+$(function() {
+  $( ".window" ).draggable();
+  $( "#iconbar" ).draggable();
+
+});
+  $(".window").click(function() {
+  $(".window").removeClass("focus");
+  $(".window").addClass("unfocus");
+  $(this).removeClass("unfocus");
+  $(this).addClass("focus");
+});
+$( "html" ).keydown(function( event ) {
+  if (event.which != 116 && event.which != 122 && event.which != 123 ) {
+    event.preventDefault();
+  }
+
+  if (event.which == 160) {
+    $("#iconbar").toggleClass("active");
+  }
+  if ($("#iconbar").hasClass( "active" )) {
+    if (event.which >=48 && event.which <= 57 ) {
+      console.log("starte programm " + (event.which - 48));
+      $("#iconbar").toggleClass("active");
+    }
+  }
+  var msg = "Handler for .keypress() called " + event.which;
+  console.log( msg);
+  console.log( event );
+});
