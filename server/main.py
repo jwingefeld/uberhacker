629a0115d32ad75c08717c769e4ed4c5906d4894
leftshift <leftshiftlp@gmail.com>
2016-06-25 20:33:21 +0200

    started building api

--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
 import os
 from flask import Flask
+
+from api import api
+
 from games.typeracer import typeracer
 
+# Initializes Flask app.
+# static_url_path='' causes all files from static_folder to be served. (e.g. /index.html)
 app = Flask(__name__, static_folder="../client", static_url_path='')
+
+
+app.register_blueprint(api, url_prefix="/api")
 
 games_base_url = '/games/'
 
@@ -14,4 +22,4 @@ def index():
     return app.send_static_file("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run('0.0.0.0')
